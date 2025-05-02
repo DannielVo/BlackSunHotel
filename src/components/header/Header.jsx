b1bb7c5db25ba956ec4ed736510e442c70f08f0b
DannielVo <phattinhoc2017@gmail.com>
2025-05-02 20:19:20 +0700

    Fix: update new Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,11 +29,15 @@ const Header = () => {
         </div>
 
         <div className="nav-group right">
+          <Link className="nav-link" to={"/profile"}>
+            Profile
+          </Link>
+          <Link className="nav-link" to={"/booking-history"}>
+            My Bookings
+          </Link>
+          <Link className="nav-link">Logout</Link>
           <Link className="nav-link" to={"/login"}>
             Login
-          </Link>
-          <Link className="nav-link" to={"/signup"}>
-            Sign up
           </Link>
         </div>
       </header>
